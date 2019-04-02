c7f635095d65323f1c9917543dab9c7063e7b8e0
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2019-04-02 19:00:08 +0800

    UPDATE

--- mongols_route.py/setup.py
+++ mongols_route.py/setup.py
@@ -8,14 +8,14 @@ import mongols_route
 
 setup(name='mongols_route',
       version=mongols_route.__version__,
-      description='Fast and simple web framework for hi-nginx',
+      description='route for pymongols',
       author=mongols_route.__author__,
       author_email=mongols_route.__author__,
-      url='https://www.hi-nginx.com/',
+      url='https://mongols.hi-nginx.com/doc/python.html',
       py_modules=['mongols_route'],
       packages= find_packages(),
       install_requires=['Jinja2>=2'],
       scripts=['mongols_route.py'],
-      license='GPL-v3',
+      license='MPL-v3',
       platforms='LINUX',
       )
